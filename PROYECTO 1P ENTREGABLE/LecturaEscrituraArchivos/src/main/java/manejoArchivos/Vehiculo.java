468f2b4608cce4a60058deb4aa90f946904a791a
Angello Vasconez <alvascon@espol.edu.ec>
2023-06-24 14:59:48 -0500

    Creacion de la clase Vehiculo.

---
+++ PROYECTO 1P ENTREGABLE/LecturaEscrituraArchivos/src/main/java/manejoArchivos/Vehiculo.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package manejoArchivos;
+
+/**
+ *
+ * @author vv
+ */
+public class Vehiculo {
+    
+    private String duenho;
+    private char placa;
+    private int modelo;
+    private int año;
+    private String chasis;
+    private String color;
+    private boolean multa;
+
+    //Constructor
+    public Vehiculo(String duenho, char placa, int modelo, int año, String chasis, String color, boolean multa) {
+        this.duenho = duenho;
+        this.placa = placa;
+        this.modelo = modelo;
+        this.año = año;
+        this.chasis = chasis;
+        this.color = color;
+        this.multa = multa;
+    }
+
+    //Getters y Setters 
+    public String getDuenho() {
+        return duenho;
+    }
+
+    public void setDuenho(String duenho) {
+        this.duenho = duenho;
+    }
+
+    public char getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(char placa) {
+        this.placa = placa;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getChasis() {
+        return chasis;
+    }
+
+    public void setChasis(String chasis) {
+        this.chasis = chasis;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isMulta() {
+        return multa;
+    }
+
+    public void setMulta(boolean multa) {
+        this.multa = multa;
+    }
+}
